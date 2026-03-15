31a03e23be2e2a59ef89c1313ba7cd587f73fcaf
AndresOrtega1509 <andresf.ortegam@uqvirtual.edu.co>
2026-03-15 14:36:08 -0500

    Se creó la funcionalidad 2: Registrar la Información de un Producto

--- modulo1/sistema_gestion_productos/src/App.java
+++ modulo1/sistema_gestion_productos/src/App.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class App {
+    static Scanner sc = new Scanner(System.in);
+    static String nombre = "N/A";
+    static double precioUnitario = -1;
+    static int cantidad = -1;
     public static void main(String[] args) throws Exception {
         mostrarMenu();
+        registrarProducto();
     }
 
     public static void mostrarMenu() {
@@ -15,5 +22,59 @@ public class App {
                 Ingrese su opción:
                 """;
         System.out.println(opciones);
+    }
+
+    public static void registrarProducto(){
+        if (validarProductoRegistrado()) {
+            System.out.print("Ingrese el nombre del producto: ");
+            nombre = sc.nextLine();
+            precioUnitario = solicitarPrecioUnitario();
+            cantidad = solicitarStock();
+        }else{
+            System.out.println("Registro de producto cancelado.");
+        }
+    }
+
+    public static boolean validarProductoRegistrado(){
+        if (!nombre.equals("N/A")) {
+            System.out.print("Ya existe un producto registrado. Dese sobreescribir los datos? (s/n)");
+            String respuesta = sc.nextLine();
+            if (respuesta.equalsIgnoreCase("s")) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static double solicitarPrecioUnitario(){
+        double precio;
+        do{
+            System.out.print("Ingrese el precio unitario: ");
+            precio = sc.nextDouble();
+            if (!esPrecioValido(precio)) {
+                System.out.println("El precio unitario debe ser mayor a 0. Intentelo nuevamente.");
+            }
+        }while(!esPrecioValido(precio));
+        return precio;
+    }
+
+    public static int solicitarStock(){
+        int stock;
+        do{
+            System.out.print("Ingrese la cantidad en stock: ");
+            stock = sc.nextInt();
+            if (!esCantidadValida(stock)) {
+                System.out.println("El cantidad debe ser un número entero no negativo. Intentelo nuevamente.");
+            }
+        }while(!esCantidadValida(stock));
+        return stock;
+    }
+
+    public static boolean esPrecioValido(double precio){
+        return precio > 0;
+    }
+
+    public static boolean esCantidadValida(int stock){
+        return stock >= 0;
     }
 }
