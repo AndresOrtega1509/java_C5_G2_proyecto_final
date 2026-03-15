f1989c7cf9f18f03582c0b49135da092e69bb47a
AndresOrtega1509 <andresf.ortegam@uqvirtual.edu.co>
2026-03-15 12:55:03 -0500

    Se creó la funcionalidad 1: Mostrar Menú Principal

--- modulo1/sistema_gestion_productos/src/App.java
+++ modulo1/sistema_gestion_productos/src/App.java
@@ -1,5 +1,19 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        mostrarMenu();
+    }
+
+    public static void mostrarMenu() {
+        String opciones = """
+                --- Sistema de Gestión de Productos ---
+                1. Registrar nuevo producto
+                2. Mostrar información del producto actual
+                3. Calcular valor total del inventario
+                4. Mostrar resumen completo del producto
+                5. Limpiar datos del producto actual
+                0. Salir
+                Ingrese su opción:
+                """;
+        System.out.println(opciones);
     }
 }
