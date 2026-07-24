740078093c9a0e133115be21a3e19e516398c5ea
AndresOrtega1509 <andresf.ortegam@uqvirtual.edu.co>
2026-07-24 15:53:19 -0500

    Proyecto Netflix Analytics terminado

--- modulo4/netflix-analytics/src/main/java/com/devsenior/andres/Main.java
+++ modulo4/netflix-analytics/src/main/java/com/devsenior/andres/Main.java
@@ -1,7 +1,14 @@
 package com.devsenior.andres;
 
+import com.devsenior.andres.repository.DataRepository;
+import com.devsenior.andres.service.AnalyticsService;
+import com.devsenior.andres.ui.ConsoleUI;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        DataRepository dataRepository = new DataRepository();
+        AnalyticsService analyticsService = new AnalyticsService(dataRepository);
+        ConsoleUI consoleUI = new ConsoleUI(analyticsService);
+        consoleUI.iniciar();
     }
 }
