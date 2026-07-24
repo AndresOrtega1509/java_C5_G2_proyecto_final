b6c2b4b3facf49cbba0599c3a65530a6da44b085
AndresOrtega1509 <andresf.ortegam@uqvirtual.edu.co>
2026-07-24 13:06:15 -0500

    Servicio terminado

--- modulo4/netflix-analytics/src/main/java/com/devsenior/andres/service/AnalyticsService.java
+++ modulo4/netflix-analytics/src/main/java/com/devsenior/andres/service/AnalyticsService.java
@@ -1,5 +1,66 @@
 package com.devsenior.andres.service;
 
-public class AnalyticsService {
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+import com.devsenior.andres.model.Genero;
+import com.devsenior.andres.model.Pelicula;
+import com.devsenior.andres.model.Usuario;
+import com.devsenior.andres.model.Visualizacion;
+import com.devsenior.andres.repository.DataRepository;
+
+public class AnalyticsService {
+    private DataRepository dataRepository;
+
+    public AnalyticsService(DataRepository dataRepository){
+        this.dataRepository = dataRepository;
+    }
+
+    public List<Map.Entry<Pelicula, Long>> mostrarPeliculasMasVistas(){
+        var resultado = dataRepository.visualizaciones
+                            .stream()
+                            .collect(Collectors.groupingBy(Visualizacion::getPelicula, Collectors.counting()))
+                            .entrySet()
+                            .stream()
+                            .sorted(Map.Entry.<Pelicula, Long>comparingByValue().reversed())
+                            .toList();
+        return resultado;             
+    }
+
+    public List<Map.Entry<Genero, List<Pelicula>>> agruparPeliculasPorGenero() {
+        var resultado = dataRepository.peliculas
+                            .stream()
+                            .collect(Collectors.groupingBy(Pelicula::getGenero))
+                            .entrySet()
+                            .stream()
+                            .toList();
+        return resultado;
+    }
+
+    public Map<Usuario, Integer> obtenerTiempoTotalVistoPorUsuario(){
+        var resultado = dataRepository.visualizaciones
+                      .stream()
+                      .collect(Collectors.groupingBy(u -> u.getUsuario(), 
+                               Collectors.summingInt(Visualizacion::getMinutosVistos)));
+        return resultado;
+    }
+
+    public List<Map.Entry<Usuario, Integer>> obtenerTopUsuariosVisualizacionesPeliculas(){
+        var resultado = obtenerTiempoTotalVistoPorUsuario()
+                      .entrySet()
+                      .stream()
+                      .sorted(Map.Entry.<Usuario, Integer>comparingByValue().reversed())
+                      .limit(3)
+                      .toList();
+        return resultado;
+    }
+
+    public Map<Genero, Double> obtenerPromedioDuracionPeliculasPorGenero(){
+        var resultado = dataRepository.peliculas
+                                      .stream()
+                                      .collect(Collectors.groupingBy(Pelicula::getGenero, 
+                                        Collectors.averagingInt(Pelicula::getDuracion)));
+        return resultado;
+    }
 }
