94d27980dae0da5e27c6bb790bc8e75a9221dd98
AndresOrtega1509 <andresf.ortegam@uqvirtual.edu.co>
2026-08-04 14:01:26 -0500

    Clases modelo finalizadas

--- modulo5/transport-fare/src/main/java/com/devsenior/andres/transport_fare/model/FareType.java
+++ modulo5/transport-fare/src/main/java/com/devsenior/andres/transport_fare/model/FareType.java
@@ -1,5 +1,7 @@
 package com.devsenior.andres.transport_fare.model;
 
 public enum FareType {
-
+    ECONOMY,
+    PREMIUM,
+    SURGE
 }
--- modulo5/transport-fare/src/main/java/com/devsenior/andres/transport_fare/model/Trip.java
+++ modulo5/transport-fare/src/main/java/com/devsenior/andres/transport_fare/model/Trip.java
@@ -1,5 +1,66 @@
 package com.devsenior.andres.transport_fare.model;
 
 public class Trip {
+    private Long id;
+    private double distance;
+    private double duration;
+    private FareType fareType;
+    private String userEmail;
+    private double totalFare;
 
+    public Trip() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    public void setDuration(double duration) {
+        this.duration = duration;
+    }
+
+    public FareType getFareType() {
+        return fareType;
+    }
+
+    public void setFareType(FareType fareType) {
+        this.fareType = fareType;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public double getTotalFare() {
+        return totalFare;
+    }
+
+    public void setTotalFare(double totalFare) {
+        this.totalFare = totalFare;
+    }
+
+    @Override
+    public String toString() {
+        return "Trip [id=" + id + ", distance=" + distance + ", duration=" + duration + ", fareType=" + fareType
+                + ", userEmail=" + userEmail + ", totalFare=" + totalFare + "]";
+    }
 }
