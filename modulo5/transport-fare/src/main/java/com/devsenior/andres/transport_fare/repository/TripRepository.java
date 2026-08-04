4daae3c3fab034183ac3514908bacdcfa9ec30b5
AndresOrtega1509 <andresf.ortegam@uqvirtual.edu.co>
2026-08-04 16:24:40 -0500

    Repository finalizado

--- modulo5/transport-fare/src/main/java/com/devsenior/andres/transport_fare/repository/TripRepository.java
+++ modulo5/transport-fare/src/main/java/com/devsenior/andres/transport_fare/repository/TripRepository.java
@@ -1,5 +1,25 @@
 package com.devsenior.andres.transport_fare.repository;
 
-public class TripRepository {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
+import com.devsenior.andres.transport_fare.model.Trip;
+
+@Repository
+public class TripRepository {
+    private final List<Trip> trips = new ArrayList<>();
+    private Long idCounter = 1L;
+
+    //agregamos un id manualmente
+    public Trip save(Trip trip){
+        trip.setId(idCounter++);
+        this.trips.add(trip);
+        return trip;
+    }
+
+    public List<Trip> findAll(){
+        return this.trips;
+    }
 }
